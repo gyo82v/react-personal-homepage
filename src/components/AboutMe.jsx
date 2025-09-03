1a2993500bae33fe7e174b545558c595c721ec44
gyo82v <130491738+gyo82v@users.noreply.github.com>
2025-09-03 20:05:49 +0200

    style: add style to the AboutMe.jsx

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -2,27 +2,31 @@ import { Link } from "react-router-dom"
 import man from "../images/man.png"
 
 export default function AboutMe(){
-    const section = `flex flex-col gap-4 flex-1 md:flex-row shadow-all rounded-lg 
-                     p-4 `
-    const p = `text-neutral-600`
-    const link = `inline-flex items-center px-4 py-2 rounded-lg ml-1
+    const section = `flex flex-col gap-4 flex-1 shadow-all rounded-lg 
+                     p-4 lg:gap-12  `
+    const p = `text-neutral-600 lg:text-lg`
+    const img = `w-32 h-32 rounded-lg object-cover flex-shrink-0 
+                 shadow-lg shadow-neutral-700/30 lg:mt-6 lg:mb-6
+                 sm:w-40 sm:h-40 lg:w-58 lg:h-58`
+    const link = `inline-flex items-center px-4 py-2 rounded-lg ml-1 lg:ml-5
                   green-cyan text-white font-medium 
                   shadow-lg transition-colors transition-transform transition-shadow
                   duration-300 ease-in-out 
                   hover:bg-gradient-to-r hover:from-cyan-500 hover:to-green-400
                   hover:shadow-xl hover:scale-105 active:scale-95`
-    const div = `flex flex-col gap-3`
+    const div = `flex flex-col gap-3 sm:gap-5 lg:gap-8`
     const span = `font-bold text-neutral-800`
+    const extraDiv = `hidden lg:flex lg:flex-col lg:gap-8 `
     return(
         <section className={section}>
             <img
                src={man}
                alt="Portrait"
-               className="w-32 h-32 rounded-lg object-cover flex-shrink-0"
+               className={img}
             />
             <div className={div}>
                 <p className={p}>
-                  <span className="font-bold mr-1 text-neutral-800 ">Hello!</span> I'm Giorgio, a web developer passionate about
+                  <span className={`${span} mr-1`}>Hello!</span> I'm Giorgio, a web developer passionate about
                   creating beautiful and functional websites.
                 </p>
                 <p className={p}>
@@ -30,7 +34,19 @@ export default function AboutMe(){
                   <span className={span}> Firebase</span> and various
                   web technologies. I love turning ideas into reality through code.
                 </p>
-                <p className={p}>Check out <Link className={link} to="/resume">my resume here</Link>.</p>
+                 <div className={extraDiv}>
+                  <p className={p}>
+                     I’ve recently completed <span className={span}>project1</span>,
+                     <span className={span}>project2</span>, and <span className={span}>project3</span>,
+                     each helping me grow as a developer and explore new creative possibilities.
+                  </p>
+                  <p className={p}>
+                     Currently, I’m diving into <span className={span}>Node.js</span>,
+                     <span className={span}>Next.js</span>, and <span className={span}>TypeScript</span>,
+                     expanding my toolkit to build modern, scalable, and efficient applications.
+                  </p>
+                </div>
+                <p className={`${p} mt-16 lg:font-bold lg:text-xl`}>Check out <Link className={link} to="/resume">my resume here</Link></p>
             </div>
         </section>
     )
