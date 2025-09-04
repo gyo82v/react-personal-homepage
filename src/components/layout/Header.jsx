5e1ea4fc06e571aafa954823915aecbdbc6f3662
gyo82v <130491738+gyo82v@users.noreply.github.com>
2025-09-04 20:16:51 +0200

    stles: improve header style

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -3,17 +3,18 @@ import { NavLink } from "react-router-dom"
 export default function Header(){
     const header = `bg-neutral-700 text-neutral-300 px-4 py-6 shadow-lg shadow-neutral-700/30
                     flex justify-between items-center`
-    const h1 = `text-2xl font-bold`
-    const nav = `flex gap-3 font-semibold`
-    const link = ``
+    const h1 = `text-2xl font-bold lg:text-3xl`
+    const nav = `flex gap-3 font-semibold lg:gap-14 lg:mr-10 lg:text-lg`
+    const link = `transition-transform duration-300 ease-in-out hover:scale-120`
+    const linkActive = `underline`
     return(
         <header className={header}>
             <h1 className={h1}>Giorgio Valle</h1>
             <nav className={nav}>
-                <NavLink to="/" className={link}>Home</NavLink>
-                <NavLink to="projects" className={link}>Projects</NavLink>
-                <NavLink to="skills" className={link}>Skills</NavLink>
-                <NavLink to="resume" className={link}>Resume</NavLink>
+                <NavLink to="/" className={(({isActive}) => isActive ? linkActive : link)}>Home</NavLink>
+                <NavLink to="projects" className={({isActive}) => isActive ? linkActive : link}>Projects</NavLink>
+                <NavLink to="skills" className={({isActive}) => isActive ? linkActive : link}>Skills</NavLink>
+                <NavLink to="resume" className={({isActive}) => isActive ? linkActive : link}>Resume</NavLink>
             </nav>
         </header>
     )
